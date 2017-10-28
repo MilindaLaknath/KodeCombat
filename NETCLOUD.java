a27ed47d7c6b78acc1efedc7c867de5c018f9b8c
MilindaLaknath <milindalaknath@gmail.com>
2017-10-28 15:57:28 +0530

    R3

--- NETCLOUD.java
+++ NETCLOUD.java
@@ -1,141 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2001-2017 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
  */
 
-
-import java.awt.Color;
-import robocode.AlphaBot;
 import robocode.HitRobotEvent;
 import robocode.ScannedRobotEvent;
-import robocode.WinEvent;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
+import java.awt.Color;
+import robocode.AlphaBot;
+
+import java.awt.*;
+import robocode.BravoBot;
+
 
 /**
+ * Walls - a sample robot by Mathew Nelson, and maintained by Flemming N. Larsen
+ * <p>
+ * Moves around the outer edge with the gun facing in.
  *
- * @author falcon
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
  */
-public class NETCLOUD extends AlphaBot{
-    int count = 0; // Keeps track of how long we've
-	// been searching for our target
-	double gunTurnAmt; // How much to turn our gun when searching
-	String trackName; // Name of the robot we're currently tracking
+public class gaiya_wall extends BravoBot {
+
+	boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
 
 	/**
-	 * run:  Tracker's main run function
+	 * run: Move around the walls
 	 */
 	public void run() {
 		// Set colors
-		setBodyColor(new Color(128, 128, 50));
-		setGunColor(new Color(50, 50, 20));
-		setRadarColor(new Color(200, 200, 70));
-		setScanColor(Color.white);
-		setBulletColor(Color.blue);
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.orange);
+		setBulletColor(Color.cyan);
+		setScanColor(Color.cyan);
 
-		// Prepare gun
-		trackName = null; // Initialize to not tracking anyone
-		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
-		gunTurnAmt = 10; // Initialize gunTurn to 10
+		// Initialize moveAmount to the maximum possible for this battlefield.
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+		peek = false;
 
-		// Loop forever
+		// turnLeft to face a wall.
+		// getHeading() % 90 means the remainder of
+		// getHeading() divided by 90.
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
+
 		while (true) {
-			// turn the Gun (looks for enemy)
-			turnGunRight(gunTurnAmt);
-			// Keep track of how long we've been looking
-			count++;
-			// If we've haven't seen our target for 2 turns, look left
-			if (count > 2) {
-				gunTurnAmt = -10;
-			}
-			// If we still haven't seen our target for 5 turns, look right
-			if (count > 5) {
-				gunTurnAmt = 10;
-			}
-			// If we *still* haven't seen our target after 10 turns, find another target
-			if (count > 11) {
-				trackName = null;
-			}
+			// Look before we turn when ahead() completes.
+			peek = true;
+			// Move up the wall
+			ahead(moveAmount);
+			// Don't look now
+			peek = false;
+			// Turn to the next wall
+			turnRight(90);
 		}
 	}
 
 	/**
-	 * onScannedRobot:  Here's the good stuff
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-
-		// If we have a target, and this isn't it, return immediately
-		// so we can get more ScannedRobotEvents.
-		if (trackName != null && !e.getName().equals(trackName)) {
-			return;
-		}
-
-		// If we don't have a target, well, now we do!
-		if (trackName == null) {
-			trackName = e.getName();
-			out.println("Tracking " + trackName);
-		}
-		// This is our target.  Reset count (see the run method)
-		count = 0;
-		// If our target is too far away, turn and move toward it.
-		if (e.getDistance() > 300) {
-			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-
-			turnGunRight(gunTurnAmt); // Try changing these to setTurnGunRight,
-			turnRight(e.getBearing()); // and see how much Tracker improves...
-			// (you'll have to make Tracker an AdvancedRobot)
-			ahead(e.getDistance() - 0);
-                        
-                        fire(1);
-                        turnGunRight(10);
-                        fire(1);
-                        turnGunLeft(10);
-                        
-			return;
-		}
-
-		// Our target is close.
-		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-		turnGunRight(gunTurnAmt);
-		fire(3);
-
-		// Our target is too close!  Back up.
-		if (e.getDistance() < 100) {
-			if (e.getBearing() > -90 && e.getBearing() <= 90) {
-				back(40);
-			} else {
-				ahead(40);
-			}
-		}
-		scan();
-	}
-
-	/**
-	 * onHitRobot:  Set him as our new target
+	 * onHitRobot:  Move away a bit.
 	 */
 	public void onHitRobot(HitRobotEvent e) {
-		// Only print if he's not already our target.
-		if (trackName != null && !trackName.equals(e.getName())) {
-			out.println("Tracking " + e.getName() + " due to collision");
+		// If he's in front of us, set back up a bit.
+		if (e.getBearing() > -90 && e.getBearing() < 90) {
+			back(100);
+		} // else he's in back of us, so set ahead a bit.
+		else {
+			ahead(100);
 		}
-		// Set the target
-		trackName = e.getName();
-		// Back up a bit.
-		// Note:  We won't get scan events while we're doing this!
-		// An AdvancedRobot might use setBack(); execute();
-		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-		turnGunRight(gunTurnAmt);
-		fire(3);
-		back(50);
 	}
 
 	/**
-	 * onWin:  Do a victory dance
+	 * onScannedRobot:  Fire!
 	 */
-	public void onWin(WinEvent e) {
-		for (int i = 0; i < 50; i++) {
-			turnRight(30);
-			turnLeft(30);
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(2);
+		// Note that scan is called automatically when the robot is moving.
+		// By calling it manually here, we make sure we generate another scan event if there's a robot on the next
+		// wall, so that we do not start moving up it until it's gone.
+		if (peek) {
+			scan();
 		}
 	}
 }
