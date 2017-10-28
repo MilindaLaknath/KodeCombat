a00fc1030216434a36f9f7d461ca0d731c9b5481
MilindaLaknath <milindalaknath@gmail.com>
2017-10-28 10:55:53 +0530

    Bot added for R1

---
+++ NETCLOUDBot.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.sun.javafx.font.FontConstants;
+import java.awt.Color;
+import robocode.AlphaBot;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
+
+/**
+ *
+ * @author falcon
+ */
+public class NETCLOUDBot extends AlphaBot{
+    boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
+
+	/**
+	 * run: Move around the walls
+	 */
+	public void run() {
+		// Set colors
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.WHITE);
+		setBulletColor(Color.cyan);
+		setScanColor(Color.cyan);
+
+		// Initialize moveAmount to the maximum possible for this battlefield.
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+		peek = false;
+
+		// turnLeft to face a wall.
+		// getHeading() % 90 means the remainder of
+		// getHeading() divided by 90.
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
+
+		while (true) {
+			// Look before we turn when ahead() completes.
+			peek = true;
+			// Move up the wall
+			ahead(moveAmount);
+			// Don't look now
+			peek = false;
+                        
+			// Turn to the next wall
+			turnRight(60);
+		}
+	}
+
+	/**
+	 * onHitRobot:  Move away a bit.
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		// If he's in front of us, set back up a bit.
+		if (e.getBearing() > -10 && e.getBearing() < 10) {
+			back(50);
+		} // else he's in back of us, so set ahead a bit.
+		else {
+			ahead(50);
+		}
+	}
+
+	/**
+	 * onScannedRobot:  Fire!
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+		// Note that scan is called automatically when the robot is moving.
+		// By calling it manually here, we make sure we generate another scan event if there's a robot on the next
+		// wall, so that we do not start moving up it until it's gone.
+		if (peek) {
+			scan();
+		}
+	}
+
+        
+}
